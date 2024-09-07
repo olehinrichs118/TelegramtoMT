482e81c9823b2688d307be34d1e1e0772624ab79
olehinrichs118 <120103837+olehinrichs118@users.noreply.github.com>
2024-09-08 01:04:23 +0200

    Update run.py
    hard coded position size 0.05

--- run.py
+++ run.py
@@ -135,7 +135,9 @@ def GetTradeInformation(update: Update, trade: dict, balance: float) -> None:
     stopLossPips = abs(round((trade['StopLoss'] - trade['Entry']) / multiplier))
 
     # calculates the position size using stop loss and RISK FACTOR
-    trade['PositionSize'] = math.floor(((balance * trade['RiskFactor']) / stopLossPips) / 10 * 100) / 100
+    # trade['PositionSize'] = math.floor(((balance * trade['RiskFactor']) / stopLossPips) / 10 * 100) / 100
+    trade['PositionSize'] = 0.05
+
 
     # calculates the take profit(s) in pips
     takeProfitPips = []
